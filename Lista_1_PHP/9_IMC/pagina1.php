a07c5d8c0c4e56bdeb3af3bce25735d0c914f94e
clara <clarabkistner@gmail.com>
2025-12-04 02:30:08 -0300

    metade de php

---
+++ Lista_1_PHP/9_IMC/pagina1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página 1</title>
+</head>
+<body>
+    <form action="9_IMC/pagina2.php" method="GET"></form>
+    <label for="nome">Nome: </label>
+    <input type="text" name="nome">
+    <label for="email">E-mail: </label>
+    <input type="text" name="email">
+    <input type="submit" value="Próximo">
+</body>
+</html>
---
+++ Lista_1_PHP/9_IMC/pagina2.php
@@ -0,0 +1,21 @@
+<?php
+    $nome = $_GET["nome"];
+    $email = $_GET["email"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página 2</title>
+</head>
+<body>
+    <form action="pagina3.php" method="POST">
+    <label for="peso">Peso (kg): </label>
+    <input type="number" name="peso">
+    <label for="altura">Altura (m): </label>
+    <input type="number" step="0.01" name="altura">
+    <input type="submit" value="Próximo">
+    </form>
+</body>
+</html>
